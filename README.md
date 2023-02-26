e7e5234386d2195db083152e520cf24371ec1216
Anej Gorkič <anej.gorkic@gmail.com>
2023-02-26 17:28:56 +0100

    Add file and ext filter config

--- README.md
+++ README.md
@@ -4,20 +4,27 @@ Validate file line-by-line against a regex pattern of your choice. Each offendin
 
 ## Features
 
-TODO: describe main features
+- highlight offending lines if they fail to match your custom regex pattern
+- specify which files you want to validate
 
 ## Requirements
 
-You need to configure a regex pattern that will be used to validate each line in the file.
+You need to configure a regex string pattern that will be used to validate each line in the file.
 
 ## Extension Settings
 
 This extension contributes the following settings:
 
-- `lineGuard.regex`: Regular expression. By default a "match-nothing" regex is set.
+- `lineGuard.regex`: Regex string. Required if you want the extension to trigger validation. (example input: `^.{1,3}$` the line will match any string that is 3 characters in length, otherwise it is an invalid line. Use `^$` to match the entire line exactly, not just containing characters)
+- `lineGuard.fileName`: Comma-delimited file name list of files that are meant to be validated. Use '\*' to include all. File names are case-sensitive. (example value: myFile.txt, anotherFile.db)
+- `lineGuard.fileExtensions`: Comma-delimited file extensions list of file types that are meant to be validated. Use '\*' to include all. Extensions are case-sensitive. (example value: .txt, .db)
 
 ## Release Notes
 
 ### 1.0.0
 
 Initial release
+
+### 1.0.1
+
+Validate only specified file names or specified file extensions.
--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as path from "path";
 
 const decorationType = vscode.window.createTextEditorDecorationType({
   isWholeLine: true,
@@ -6,23 +7,66 @@ const decorationType = vscode.window.createTextEditorDecorationType({
   overviewRulerColor: "rgba(255, 32, 21)",
   overviewRulerLane: vscode.OverviewRulerLane.Left,
   light: {
-    backgroundColor: "rgba(255, 105, 97)",
-    color: "white",
+    textDecoration: "underline rgba(255, 105, 97, 0.8) 1px wavy",
   },
   dark: {
-    backgroundColor: "rgb(255, 105, 97)",
-    color: "white",
+    textDecoration: "underline rgba(255, 105, 97, 0.8) 1px wavy",
   },
 });
+
+function shouldValidateDocument(
+  activeEditor: vscode.TextEditor,
+  config: vscode.WorkspaceConfiguration
+) {
+  const regex = config.get("regex") as string;
+  const fileName = path.basename(activeEditor.document.fileName);
+  const fileNameConf = (config.get("fileNames") as string).trim();
+
+  const extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+  const extensionConf = (config.get("fileExtensions") as string).trim();
+
+  const fileNames: string[] =
+    fileNameConf === ""
+      ? []
+      : fileNameConf.split(",").map((fileName) => fileName.trim());
+  const fileExtensions: string[] =
+    extensionConf === ""
+      ? []
+      : extensionConf.split(",").map((fileName) => fileName.trim());
+
+  const isRegexSet = regex !== "";
+  const isFileNameMatch = fileNames.includes(fileName);
+  const isExtensionMatch = fileExtensions.includes(`.${extension}`);
+
+  if (!isRegexSet) {
+    return false;
+  }
+
+  if (fileNames.length > 0) {
+    if (fileNameConf === "*" || isFileNameMatch) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (fileExtensions.length > 0) {
+    if (extensionConf === "*" || isExtensionMatch) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return false;
+}
 
 function validateLine(
   lineText: string,
   lineNumber: number,
-  regex: RegExp
+  regExp: RegExp
 ): vscode.Diagnostic | null {
-  const r = new RegExp(regex);
-
-  if (r.test(lineText)) {
+  if (!regExp.test(lineText)) {
     const range = new vscode.Range(lineNumber, 0, lineNumber, lineText.length);
 
     return new vscode.Diagnostic(
@@ -35,48 +79,74 @@ function validateLine(
   return null;
 }
 
-function validateDocument(document: vscode.TextDocument) {
-  const config = vscode.workspace.getConfiguration("lineGuardian");
+function validateDocument(
+  editor: vscode.TextEditor,
+  document: vscode.TextDocument
+) {
+  const config = vscode.workspace.getConfiguration("lineGuard");
   const regex = config.get("regex") as string;
-  const { lineCount } = document;
 
-  const diagnostics: vscode.Diagnostic[] = [];
-
-  for (let lineNumber = 0; lineNumber < lineCount; lineNumber++) {
-    const lineText = document.lineAt(lineNumber).text;
-    const diagnostic = validateLine(lineText, lineNumber, regex);
-
-    if (diagnostic) {
-      diagnostics.push(diagnostic);
-    }
+  if (regex === "") {
+    vscode.window.showErrorMessage(
+      "No validation regex set. Please set 'lineGuard.regex' configuration option."
+    );
+    return;
   }
 
-  const decorations = diagnostics.map((diagnostic) => diagnostic.range);
+  if (editor) {
+    if (shouldValidateDocument(editor, config)) {
+      try {
+        const regExp = new RegExp(regex);
 
-  vscode.window.activeTextEditor?.setDecorations(decorationType, decorations);
+        const { lineCount } = document;
+        const diagnostics: vscode.Diagnostic[] = [];
+
+        for (let lineNumber = 0; lineNumber < lineCount; lineNumber++) {
+          const lineText = document.lineAt(lineNumber).text;
+          const diagnostic = validateLine(lineText, lineNumber, regExp);
+
+          if (diagnostic) {
+            diagnostics.push(diagnostic);
+          }
+        }
+
+        const decorations = diagnostics.map((diagnostic) => diagnostic.range);
+        editor.setDecorations(decorationType, decorations);
+      } catch (error) {
+        vscode.window.showErrorMessage(
+          "Failed to parse file. Check plugin settings."
+        );
+      }
+    }
+  }
 }
 
 function validateAllOpenDocuments() {
-  for (const document of vscode.workspace.textDocuments) {
-    validateDocument(document);
+  const editors = vscode.window.visibleTextEditors;
+
+  for (const editor of editors) {
+    validateDocument(editor, editor.document);
   }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log("activated");
-
   context.subscriptions.push(
     vscode.workspace.onDidChangeTextDocument(({ document }) => {
-      if (vscode.window.activeTextEditor?.document === document) {
-        validateDocument(document);
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        validateDocument(editor, document);
       }
     })
   );
 
   context.subscriptions.push(
-    vscode.workspace.onDidOpenTextDocument((document) => {
-      validateDocument(document);
-    })
+    vscode.window.onDidChangeActiveTextEditor(
+      (editor: vscode.TextEditor | undefined) => {
+        if (editor) {
+          validateDocument(editor, editor.document);
+        }
+      }
+    )
   );
 
   validateAllOpenDocuments();
